32a63b8b3cf50dc4ed795756913f3b58f18a137b
Kirandhanwai <kirandhanwai123@gmail.com>
2022-10-14 23:20:54 +0530

    Added :Calculation Line Comparision using Object Orinted Program

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,39 +4,19 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class LineComparison {
-	public static int lineLengthCal() {
-		int x1,y1,x2,y2;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the co-ordinates of line!!: x1,y1,x2,y2"); 
-		x1 = scanner.nextInt();
-		y1 = scanner.nextInt();
-		x2 = scanner.nextInt();
-		y2 = scanner.nextInt();
-		int lineLength = (int)Math.sqrt(Math.pow((x1-x2), 2) 
-				+ Math.pow((y1-y2), 2) );
-		return lineLength;
-		
-	}
-	public static void equalityCheck(int lineOneLength , int lineTwoLength ) {
-		if(lineOneLength == lineTwoLength ) {
-			System.out.println("Lines are Equal");
-		}
-		else if(lineOneLength > lineTwoLength){
-			System.out.println("Line 1 is greater than Line 2 ");
-		}
-		else {
-			System.out.println("Line 2 is greater than Line 1 ");
-		}
-	}
-
-	public static void main(String[] args) {
-		int lineOneLength = lineLengthCal();
-		int lineTwoLength = lineLengthCal();
-		System.out.println("The length of line 1 is: "+lineOneLength);
-		System.out.println("The length of line 2 is: "+lineTwoLength);
-		equalityCheck(lineOneLength,lineTwoLength);
-		
-
-	}
 	
+    public static void main(String[] args) {
+    	
+    	Line line1 = new Line();
+    	System.out.println("Enter the points for line 1 : ");
+    	line1.pointsInput();
+    	Line line2 = new Line();
+     	System.out.println("Enter the points for line 2 : ");
+    	line2.pointsInput();
+    
+    	Line.compareByEquals(line1, line2);
+    	Line.compareByCompareTo(line1, line2);
+    	
+    }
 }
+
